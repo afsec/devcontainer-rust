61067d18cd2d9f703f27b518fd7263f2926d7d9b
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-01-21 00:23:43 -0300

    Create install_tooling.sh

---
+++ install_tooling.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd /usr/local/bin
+
+# Install Conventional Commits - Cocogitto (`cog`)
+wget https://github.com/cocogitto/cocogitto/releases/download/5.2.0/cocogitto-5.2.0-x86_64-unknown-linux-musl.tar.gz -O - | zcat - | tar xvf - cog
+
+# Install Just (`just`)
+wget https://github.com/casey/just/releases/download/1.12.0/just-1.12.0-x86_64-unknown-linux-musl.tar.gz -O - | zcat - | tar xvf - just
+
+# Install Difftastic (`difft`)
+wget https://github.com/Wilfred/difftastic/releases/download/0.42.0/difft-x86_64-unknown-linux-gnu.tar.gz -O - | zcat - | tar xvf - difft
+
+# Install Cargo audit (`cargo-audit`)
+wget https://github.com/rustsec/rustsec/releases/download/cargo-audit/v0.17.4/cargo-audit-x86_64-unknown-linux-musl-v0.17.4.tgz -O - | zcat - | tar xvf - "cargo-audit-x86_64-unknown-linux-musl-v0.17.4/cargo-audit"
+
+# Install IC tooling: `ic-wasm`. 
+wget https://github.com/dfinity/ic-wasm/releases/download/0.3.2/ic-wasm-linux64 -O ic-wasm
+chmod +x ic-wasm
