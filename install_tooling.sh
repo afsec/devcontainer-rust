3dfdaf131f964b56cf4b8ea74a1ee073a0670a37
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-01-23 10:29:03 -0300

    Update install_tooling.sh

--- install_tooling.sh
+++ install_tooling.sh
@@ -11,6 +11,11 @@ wget https://github.com/casey/just/releases/download/1.12.0/just-1.12.0-x86_64-u
 # Install Difftastic (`difft`)
 wget https://github.com/Wilfred/difftastic/releases/download/0.42.0/difft-x86_64-unknown-linux-gnu.tar.gz -O - | zcat - | tar xvf - difft
 
+# Install Dfinity candid (`didcv`)
+wget https://github.com/dfinity/candid/releases/download/2022-11-17/didc-linux64
+chmod +x didc-linux64
+mv didc-linux64 didc
+
 # Install Cargo audit (`cargo-audit`)
 wget https://github.com/rustsec/rustsec/releases/download/cargo-audit/v0.17.4/cargo-audit-x86_64-unknown-linux-musl-v0.17.4.tgz -O - | zcat - | tar xvf - "cargo-audit-x86_64-unknown-linux-musl-v0.17.4/cargo-audit"
 mv ./cargo-audit-x86_64-unknown-linux-musl-v0.17.4/cargo-audit ./
